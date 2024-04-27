c433b9da2fdd65631831229c5311106a22dfc818
Kagiso Modika <kagisomodika@yahoo.com>
2024-04-27 22:57:32 +0200

    Where Lesson 3 ended.

--- Lesson 3/index.js
+++ Lesson 3/index.js
@@ -3,3 +3,21 @@ const h1 = document.createElement("h1");
 h1.textContent = "This is the header";
 h1.className = "header";
 document.getElementById("root").append(h1);
+
+// This is a declarative way of writing React.
+const element = <h1 className="header2">Declarative header</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
+
+const navigation = (
+    <nav>
+        <h1>My website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+ReactDOM.render(navigation, document.getElementById("root"));
